dc8f5d02afe974c8210fb079b104e38d7085ef1f
Akshat Shaw <shawakshat34@gmail.com>
2025-08-30 23:57:04 +0530

    Python Exercise day 5, JAVA learning day 1, Repo restructure

---
+++ JAVA-Lessons/Basics/01_Lessons/Day1/_2_Main1.java
@@ -0,0 +1,13 @@
+public class _2_Main1 {
+    public static void main(String[] args) { 
+        System.out.println("public static void main(String[] args){} is the main() method."); 
+    }
+}
+
+/*
+    Explanation :-
+    public class Main → Everything in Java is inside a class. The file name must match the class name (Main1.java → Main1 class).
+    public static void main(String[] args) → The main method is where the program starts running.
+    A class defines what something is and what it can do (its properties and behaviors).
+    I will learn about classes later in the lessons.
+ */
