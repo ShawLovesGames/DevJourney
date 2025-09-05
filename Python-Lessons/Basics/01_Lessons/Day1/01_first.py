c397b431dd34f73ba2ccda36687167bc8f825d39
Akshat Shaw <shawakshat34@gmail.com>
2025-09-05 11:06:08 +0530

    Quick Fix

--- Python-Lessons/Basics/01_Lessons/Day1/01_first.py
+++ Python-Lessons/Basics/01_Lessons/Day1/01_first.py
@@ -1,6 +1,6 @@
 # ---The first program---
 print("Hello, World!") # Basic print statement and printing a string
-print(20) # Printing a number doesn't require qoutation\
+print(20) # Printing a number doesn't require qoutation
 print("I have already learned the basics of C")
 
 # We use '#' for single line comments and can use triple double-qoutes for multi line comments
