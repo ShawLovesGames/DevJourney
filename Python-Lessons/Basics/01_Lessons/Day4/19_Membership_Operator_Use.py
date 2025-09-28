538eb4454c73c8d1eded7bfb944110c0f3721217
Akshat Shaw <shawakshat34@gmail.com>
2025-09-28 21:45:00 +0530

    Fixes #7

--- Python-Lessons/Basics/01_Lessons/Day4/19_Membership_Operator_Use.py
+++ Python-Lessons/Basics/01_Lessons/Day4/19_Membership_Operator_Use.py
@@ -4,9 +4,9 @@
 s = "I love programming"
 if "love" in s:
     print("Akshat loves programming.")
-    print("love" in s) # Prints True because the substring "love" does exist in the string s
+    print("love" in s) # Prints True because the substring "love" does exist in the string s.
 
 # not in Operator.
 if "hate" not in s:
     print("Akshat still loves programming.")
-    print("hate" not in s) # Prints True because the substring "hate" does not exist in string s
+    print("hate" not in s) # Prints True because the substring "hate" does not exist in string s.
